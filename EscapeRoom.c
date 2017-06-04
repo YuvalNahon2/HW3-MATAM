47bc41fb820a3b6feae5d8d3327ef5c0636a526c
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2017-06-05 00:01:27 +0300

    project files were set

--- EscapeRoom.c
+++ EscapeRoom.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#define POW2(x) ((x)*(x))
 #include "EscapeRoom.h"
 struct EscapeRoomS{
     int id;
@@ -10,8 +11,13 @@ struct EscapeRoomS{
     int close_hour;
     int dificulty;
 };
-MtmErrorCode escapeRoomCreate (EscapeRoom escapeRoom,Company *company, int id,
-                               int price, int num_people,int open_hour,
+
+static MtmErrorCode checkRoom(EscapeRoom escapeRoom,Costumer costumer,
+                              int day,int hour);
+
+
+MtmErrorCode escapeRoomCreate (EscapeRoom escapeRoom, Company *company, int id,
+                               int price, int num_people, int open_hour,
                                int close_hour, int diff){
     escapeRoom=malloc(sizeof(*escapeRoom));
     if(escapeRoom==NULL)
@@ -23,8 +29,16 @@ MtmErrorCode escapeRoomCreate (EscapeRoom escapeRoom,Company *company, int id,
     escapeRoom->price=price;
     escapeRoom->open_hour=open_hour;
     escapeRoom->close_hour=close_hour;
+    List ordersList;
+    ordersList=listCreate(escapeRoomOrderCopy,orderDestroy);
+    if(ordersList==NULL)
+        return MTM_OUT_OF_MEMORY;
+    escapeRoom->Orders=ordersList;
     return MTM_SUCCESS;
 
+}
+ListElement escapeRoomOrderCopy(ListElement order){
+    return (ListElement)orderCopy((Order)order);
 }
 MtmErrorCode escapeRoomDestroy(EscapeRoom escapeRoom){
     if(listGetSize(escapeRoom->Orders)>0)
@@ -35,12 +49,52 @@ MtmErrorCode escapeRoomDestroy(EscapeRoom escapeRoom){
 }
 MtmErrorCode escapeRoomOrder(EscapeRoom escapeRoom,Costumer costumer,
                              int day,int hour,int num_people){
+    MtmErrorCode check_room_avaliable;
+    check_room_avaliable=checkRoom(escapeRoom,costumer,day,hour);
+    if(check_room_avaliable!=MTM_SUCCESS){
+        return check_room_avaliable;
+    }
     Order newOrder;
-    return orderCreate(&newOrder,day,hour,&escapeRoom,&costumer,num_people);
+    newOrder=orderCreate(day,hour,&escapeRoom,&costumer,num_people);
+    if(newOrder==NULL)
+        return MTM_OUT_OF_MEMORY;
+    listInsertLast(escapeRoom->Orders,newOrder);
+    return MTM_SUCCESS;
 }
-MtmErrorCode escapeRoomRecommendScore(EscapeRoom escapeRoom,Costumer costumer,int num_people){
+static MtmErrorCode checkRoom(EscapeRoom escapeRoom,Costumer costumer,
+                              int day,int hour){
+    Order iterator;
+    iterator=listGetFirst(escapeRoom->Orders);
+    OrderTime iteratorOrderTime;
+    Costumer iteratorCostumer;
+    while(iterator!=NULL){
+        iteratorOrderTime=orderGetOrderTime(iterator);
+        iteratorCostumer=orderGetCostumer(iterator);
+        if(iteratorOrderTime.order_hour==hour &&
+           iteratorOrderTime.order_day==day){
+            if(iteratorCostumer==costumer) {
+                return MTM_CLIENT_IN_ROOM;
+            }
+            return MTM_ROOM_NOT_AVAILABLE;
+        }
+        iterator=listGetNext(escapeRoom->Orders);
+    }
+    return MTM_SUCCESS;
+}
+int escapeRoomRecommendScore(EscapeRoom escapeRoom,int costumer_level,int num_people){
+    return POW2((escapeRoom->num_people-num_people))+
+           POW2((escapeRoom->dificulty-costumer_level));
+}
+List escapeRoomEndDay(EscapeRoom escapeRoom,List *roomOrders,int money_earned,int day){
+    List todays_orders;
+    bool today=true;
+    todays_orders=listFilter(escapeRoom->Orders,orderCheckOrderday,&today);
+    today = false;
+
+    return todays_orders;
 
 }
-MtmErrorCode escapeRoomEndDay(EscapeRoom escapeRoom,List *roomOrders,int money_earned,int day){
 
+int getId(EscapeRoom escapeRoom){
+    return escapeRoom->id;
 }
--- EscapeRoom.h
+++ EscapeRoom.h
@@ -4,7 +4,7 @@
 #include "Company.h"
 typedef struct EscapeRoomS *EscapeRoom;
 #endif //HW3_ESCAPEROOM_H
-
+ListElement escapeRoomOrderCopy(ListElement order);
 MtmErrorCode escapeRoomCreate (EscapeRoom escapeRoom,Company *company, int id,
                                int price, int num_people,int open_hour,
                                int close_hour, int diff);
@@ -13,7 +13,10 @@ MtmErrorCode escapeRoomDestroy(EscapeRoom escapeRoom);
 
 MtmErrorCode escapeRoomOrder(EscapeRoom escapeRoom,Costumer costumer,
                              int day,int hour,int num_people);
-MtmErrorCode escapeRoomRecommendScore(EscapeRoom escapeRoom,Costumer costumer,int num_people);
+int escapeRoomRecommendScore(EscapeRoom escapeRoom,int costumer_level,int num_people);
 //we need to create a copy of the orders and then free the orders in the room.
 // also calculates the earnings of the room
 MtmErrorCode escapeRoomEndDay(EscapeRoom escapeRoom,List *roomOrders,int money_earned,int day);
+
+//id geter
+int getId(EscapeRoom escapeRoom);
