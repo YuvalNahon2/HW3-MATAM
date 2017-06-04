ae3dad514ff7820829a35f799fb235cfa9cb6cc1
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2017-06-05 00:10:22 +0300

    project files were set

--- Order.h
+++ Order.h
@@ -5,8 +5,16 @@
 #include "EscapeRoom.h"
 typedef struct OrderS *Order;
 #endif //HW3_ORDER_H
+typedef struct OrderTimeS{
+    int order_day;
+    int order_hour;
+}OrderTime;
+
 Order orderCreate(int order_day, int order_hour,
                          EscapeRoom *escapeRoom, Costumer *costumer,
                          int num_of_people);
-
-MtmErrorCode orderDestroy(Order order);
+Order orderCopy(Order order);
+void orderDestroy(ListElement order);
+Costumer orderGetCostumer(Order order);
+OrderTime orderGetOrderTime(Order order);
+bool orderCheckOrderday(ListElement order,void* day);
